50b3d049eae5a66d3f7f3bdd4706be4aeccd8588
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-09 12:09:44 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+// var initial=1000;
+// var getCoins=document.getElementById("grn-coins");
+// var pE1=document.getElementById("p");
+// getCoins.innerHTML=initial
+
+
+
+
+// function  playGame() {
+//     var bidAmount=+prompt("Enter Bid Ammount");
+//     var bidNumbere=+prompt("Enter Bid Number");
+//     var generateRandomNumberE1=genNum()
+
+//    var isBidMatched= bidMatch(bidNumbere,generateRandomNumberE1)
+//     bidMessage(isBidMatched,bidAmount);
+// }
+
+
+// function genNum() {
+//     var genNumbers=Math.ceil(Math.random()*3);
+//     return genNumbers;
+// }
+
+// function bidMatch( bidNumbere,generateRandomNumberE1) {
+//     var match=false;
+//     if( bidNumbere ===generateRandomNumberE1){
+//         match =true;
+//     }
+//     return match
+// }
+
+// function bidMessage(isBidMatched,bidAmount) {
+//     if(isBidMatched){
+//         initial=initial+bidAmount;
+//         getCoins.innerHTML=initial;
+//         pE1.innerHTML="Congratulations You Won the bid. "+ bidAmount +" Is Added Your Acount "
+
+//     }
+
+//     else{
+//         initial=initial-bidAmount;
+//         getCoins.innerHTML=initial;
+//         pE1.innerHTML="Sorry You Lost the bid. "+ bidAmount +" Is Dedected Your Acount "
+        
+//     }
+// }
+
+
+
+
+// var arry=[];
+// console.log(arry)
+// function register() {
+//     var userEmail=document.getElementById("email").value;
+//     var userPass=document.getElementById("password").value;
+//    var obj={
+//        userEmail,
+//        userPass
+//    }
+//    arry.push(obj);
+// }
+
+// function login() {
+//     var userEmailE1=document.getElementById("email").value;
+//     var userPassE1=document.getElementById("password").value;
+
+//     for(i=0; i<arry.length; i++){
+//        var userObj=arry[i];
+//         if (userObj.email === userEmailE1 && userObj.password === userPassE1) {
+//             console.log("use")
+//             alert("User Login")
+//         }
+//         else{
+//             alert("incorrect")
+//         }
+//     }
+  
+// }
+
+
+
+
+// var a=3;
+// var b=5;
+//             3+1 +  4
+// console.log(a++ + a)
+//             ++a
+//             1+3
+
+
+
+
+
+
+
+
+
+
+
+
+
